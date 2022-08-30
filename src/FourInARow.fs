e1e85d7812257e3e2e661a577fee8ae2ebaff3ae
john <muffinpost1@gmail.com>
2022-08-30 17:29:20 -0500

    more progress

--- src/FourInARow.fs
+++ src/FourInARow.fs
@@ -1,29 +1,39 @@
 ﻿module FourInARow
 
 type ChipType = Player1 | Player2
-type BoardSlot = Chip | Empty
+type BoardSlot = ChipType of ChipType | Empty
 type BoardColumn = int
 type Board = BoardSlot array
+type GameOverStatus =
+    | Player1Win
+    | Player2Win
+    | Tie
+type BoardStatus = 
+    | GameOver of GameOverStatus
+    | StillGoing
 type GetPlayerMove = Board -> BoardColumn
 type ApplyMove = ChipType -> BoardColumn -> Board -> Board
 type DoTurn = ChipType -> GetPlayerMove -> Board -> Board
 
 let numRows = 6
 let numCols = 7
-let board = Array2D.init<BoardSlot> numRows numCols (fun x y -> Empty)
+let emptyBoard = Array2D.init<BoardSlot> numRows numCols (fun x y -> Empty)
 
-let applyMove:ApplyMove = fun chipType boardColumn board ->
+let applyMove chipType boardColumn board =
     board
 
-let doTurn:DoTurn = fun chipType getPlayerMove board ->
+let doTurn chipType getPlayerMove board =
     let playerMove = getPlayerMove board
     board |> applyMove chipType playerMove
 
-let showTurn board:Board =
+let showTurn (board:Board) =
     printfn "%A" board
     board
 
-let rec gameLoopHelper
+let isGameOver (board:Board) =
+    false
+
+let rec gameLoop
     (getP1Move:GetPlayerMove)
     (getP2Move:GetPlayerMove)
     (board:Board) =
@@ -32,5 +42,4 @@ let rec gameLoopHelper
     |> showTurn
     |> doTurn Player2 getP2Move
     |> showTurn
-
-printfn "%A" board
+    |> gameLoop getP1Move getP2Move
