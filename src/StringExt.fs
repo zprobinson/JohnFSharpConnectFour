2ab6f98d8f109798abcd9d9e20bb19cdbdb956b5
Zach <59841145+msrobinson42@users.noreply.github.com>
2022-09-12 23:13:03 -0500

    Update StringExt.fs

--- src/StringExt.fs
+++ src/StringExt.fs
@@ -4,6 +4,7 @@ module StringExt
 // Cool function composition!
 // Notice how the types line up, which allow us to build
 // the function without ever considering the incoming data!
+// This is called writing functions in a "point-free" way
 // Array.ofList :: T list -> T[]
 // String       :: char[] -> string
 // ofList       :: char list -> string
