bb22631f9f73b3ea40d425db8cff5fc765895177
Zach <59841145+msrobinson42@users.noreply.github.com>
2022-09-12 23:10:12 -0500

    Update OOWrappers.fs

--- src/OOWrappers.fs
+++ src/OOWrappers.fs
@@ -5,7 +5,14 @@ let random = new Random();
 let randomNextInt inclusiveMin exclusiveMax =
     random.Next(inclusiveMin, exclusiveMax)
 
-let readConsoleLine () = Console.ReadLine()
+// Just like in math, if you have the same value on both sides,
+// You can subtract it from both sides and everything stays the same!
+// Here we had unit "()" on both sides, so we can get rid of it and it means the same thing.
+// Writing functions without specifying the parameters is called writing them "point-free".
+//  (unit is a parameter)
+// Point-free code can be elegant, but also confusing.
+// A fun challenge is to try and write an F# Fibonacci function as point-free as possible.
+let readConsoleLine = Console.ReadLine
 
 // I try to avoid using parens when not needed when making function calls
 // I prefer using the `var : type` declaration over `var:type` or `var: type`
